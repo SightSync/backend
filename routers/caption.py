e5a3c801c0a73603e45f01339a1bc182c555bd70
Oriol Agost Batalla <gpg18@agost.info>
2023-12-02 14:24:38 +0100

    feat: add caption service skeleton

--- routers/caption.py
+++ routers/caption.py
@@ -1,16 +1,30 @@
-from fastapi import APIRouter
+import os
+
+from fastapi import APIRouter, HTTPException
 from fastapi import status
+
+from schemas.errors import NOT_FOUND
+from services.caption import CaptionService
+from services.image import UPLOAD_IMG_DIR
 
 router = APIRouter(
     prefix="/caption",
     tags=["caption"],
 )
 
+service = CaptionService()
+
 
 @router.post(
     "/",
     status_code=status.HTTP_200_OK,
     response_model=str,
+    responses={**NOT_FOUND},
 )
-async def get_caption():
-    return {"message": "Hello World"}
+async def get_caption(
+    image_name: str,
+    query: str,
+):
+    if image_name not in os.listdir(UPLOAD_IMG_DIR):
+        raise HTTPException(status_code=404, detail="Image not found")
+    return service.get_query(image_name, query)
---
+++ services/caption.py
@@ -0,0 +1,3 @@
+class CaptionService:
+    def get_query(self, img_name: str, query: str) -> str:
+        return "Test"
