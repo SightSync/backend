7fffb9a10608a2b943801186e17e134cbddfaf7f
FerranAD <ferranaran@tuta.io>
2023-12-03 08:05:42 +0100

    hotfix

--- routers/locate.py
+++ routers/locate.py
@@ -3,7 +3,6 @@ import os
 from fastapi import APIRouter, HTTPException
 from fastapi import status
 
-from schemas.direction import Direction
 from schemas.errors import NOT_FOUND, NOT_ACCEPTABLE
 from services.image import UPLOAD_IMG_DIR
 from services.intent import IntentRecognitionService
@@ -21,17 +20,17 @@ class_extraction_service = IntentRecognitionService()
 @router.post(
     "/",
     status_code=status.HTTP_200_OK,
-    response_model=list[Direction],
+    response_model=str,
     responses={**NOT_FOUND, **NOT_ACCEPTABLE},
 )
 def locate(
         image_name: str,
-        query: str,
+        class_name: str,
 ):
     if image_name not in os.listdir(UPLOAD_IMG_DIR):
         raise HTTPException(status_code=404, detail="Image not found")
 
-    class_name = class_extraction_service.get_class_for(query)
+    class_name = class_extraction_service.get_class_for(class_name)
     if class_name == "NO CLASS" or class_name == "":
         return "The requested item is not an object or I am not sure about which object you are looking for. Please try again with another object or rephrase your question."
     direction = service.predict(image_name, class_name)
